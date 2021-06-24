6744316d615a4bf99c57852f0b90af616c651cf8
Shacklebolt13 <19cse223.gagandeepsinghbhambrah@giet.edu>
2021-06-24 06:59:00 +0000

    added static root

--- socialMediaSite/views.py
+++ socialMediaSite/views.py
@@ -8,6 +8,7 @@ from django.conf import settings
 import bcrypt
 import sys
 
+
 def index(request):
     if(checkLoginStatus(request)):
         return home(request)
@@ -16,34 +17,42 @@ def index(request):
         response.delete_cookie('email')
         return response
 
+
 def login(request):
     params={}
     return render(request,'login.html',params)
+
 
 def about(request):
     params={}
     return render(request,'about.html',params)
 
+
 def signin(request):
     params={}
     return render(request,'signin.html',params)
+
 
 def home(request):
     params={}
     return render(request,'home.html',params)
 
+
 def createId(request):
     confHidden(request)
+
     fname=request.POST.get('fname')
     lname=request.POST.get('lname')
     phone=request.POST.get('phone')
     mail=request.POST.get('mail')
-    password=hashPass(request.POST.get('pass'))
+    password=request.POST.get('pass')
     gender=request.POST.get('gender')
+
     gender=True if gender=="true" else False
     salt=bcrypt.gensalt()
     password=bcrypt.hashpw(password.encode('utf-8'),salt)
     password=str(password,encoding='utf-8')
+    
     if(gender):
         dp="images/defaultMan.png"
     else:
@@ -51,14 +60,18 @@ def createId(request):
 
     user=User(firstName=fname,lastName=lname,phNo=phone,email=mail,password=password,gender=gender,profilePicture=dp)
     user.save()
+    
     response= redirect('/')
     response.set_cookie('email',mail,7,datetime.datetime.now()+datetime.timedelta(days=7))  #expires a week later
+    
     return response
+
 
 def loginAction(request : HttpRequest):
     confHidden(request)
     mail=request.POST.get('mail',False)
     password=request.POST.get('pass',False)
+    print(mail,password)
     user=User.objects.filter(email=mail)
     if(len(user)!=0):
         hashed=user[0].password.encode('utf-8')
@@ -86,7 +99,3 @@ def checkLoginStatus(request):
         return True 
     else:
         return False
-    
-def hashPass(password :str):
-    #TODO create hashing algorithm
-    return password
